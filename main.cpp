22082e8973cf8b56652b8b449dd1055bd63ce95b
ivan peralta <ivan.peralta@udea.edu.co>
2024-08-31 20:15:12 -0500

    esqueleto

--- main.cpp
+++ main.cpp
@@ -4,6 +4,118 @@ using namespace std;
 
 int main()
 {
-    cout << "tarea en casa" << endl;
+    int opcion=1;
+    cout << "bienvenido a la practica 1" << endl;
+    while(opcion!=0)
+    {
+    cout << "seleccione una opcion: " << endl;
+    cout << "1: " << endl;
+    cout << "2: " << endl;
+    cout << "3: " << endl;
+    cout << "4: " << endl;
+    cout << "5: " << endl;
+    cout << "6: " << endl;
+    cout << "7: " << endl;
+    cout << "8: " << endl;
+    cout << "9: " << endl;
+    cout << "10: " << endl;
+    cout << "11: " << endl;
+    cout << "12: " << endl;
+    cout << "13: " << endl;
+    cout << "14: " << endl;
+    cout << "15: " << endl;
+    cout << "16: " << endl;
+    cout << "17: " << endl;
+    cin>>opcion;
+    switch (opcion) {
+    case 1:
+    {
+
+    }
+        break;
+    case 2:
+    {
+
+    }
+        break;
+    case 3:
+    {
+
+    }
+        break;
+    case 4:
+    {
+
+    }
+        break;
+    case 5:
+    {
+
+    }
+        break;
+    case 6:
+    {
+
+    }
+        break;
+    case 7:
+    {
+
+    }
+        break;
+    case 8:
+    {
+
+    }
+        break;
+    case 9:
+    {
+
+    }
+    case 10:
+    {
+
+    }
+        break;
+    case 11:
+    {
+
+    }
+        break;
+    case 12:
+    {
+
+    }
+        break;
+    case 13:
+    {
+
+    }
+        break;
+    case 14:
+    {
+
+    }
+        break;
+    case 15:
+    {
+
+    }
+        break;
+    case 16:
+    {
+
+    }
+        break;
+    case 17:
+    {
+
+    }
+        break;
+    default:
+        break;
+    }
+
     return 0;
+    }
 }
