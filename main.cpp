9473967f410f6e142c760854fab9d04cf04486c9
ivan peralta <ivan.peralta@udea.edu.co>
2024-09-04 21:00:34 -0500

    lo que se pudo

--- main.cpp
+++ main.cpp
@@ -2,15 +2,10 @@
 using namespace std;
 bool EsVocal(char caracter);
 void Combinar(int cantidad);
-bool Primo(int numero);
-int encontrarEnesimoPrimo(int n);
 int CalcularMCM(int A, int B);
 int MCD(int A, int B);
-int numeroTriangular(int n);
-int contarDivisores(int num);
-void encontrarNumeroTriangular(int k);
-int MaximoPrimo(int n);
-bool esPrimo(int num);
+int MaxPrimo(int n);
+bool Primo(int num);
 int main()
 {
     int opcion=1;
@@ -70,7 +65,7 @@ int main()
         break;
     case 3:
     {cout << "ejecutando el problema 3..."<< endl;
-        int dias_por_mes[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int diasmes[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         int mes;
         cout<<"ingrese un mes"<<endl;
         cin>>mes;
@@ -85,43 +80,43 @@ int main()
             cout<<dia<<"/"<<mes<<" es valida en biniestro"<<endl;
             return false;
         }
-        if(dia < 1 || dia > dias_por_mes[mes - 1]){
-            cout<<dia<<"/"<<mes<< " es una fecha invalida."<<endl;
+        if(dia<1 || dia>diasmes[mes - 1]){
+            cout<<dia<<"/"<<mes<<" es una fecha invalida."<<endl;
             return false;
         }
-        if((dia >= 1 && dia <= dias_por_mes[mes - 1])){
-            cout<<dia<<"/"<<mes<< " es una fecha valida."<<endl;
+        if((dia>=1 && dia<=diasmes[mes - 1])){
+            cout<<dia<<"/"<<mes<<" es una fecha valida."<<endl;
             return false;
         }
     }
         break;
     case 4:
     {cout << "ejecutando el problema 4..."<< endl;
-        int hora1;
+        int tiempo1;
         cout<<"ingrese la primer hora:";
-        cin>>hora1;
-        if(hora1>=2400 || hora1%100>=60){
+        cin>>tiempo1;
+        if(tiempo1>=2400 || tiempo1%100>=60){
             cout<<"es un tiempo invalido"<<endl;
         }
         else{
-            int hora2;
+            int tiempo2;
             cout<<"ingrese la segunda hora:";
-            cin>>hora2;
-            if(hora2>=2400 || hora2%100>=60){
+            cin>>tiempo2;
+            if(tiempo2>=2400 || tiempo2%100>=60){
                 cout<<"es un tiempo invalido"<<endl;
             }
+        else{
+            int horas=tiempo1/100+tiempo2/100;
+            int minutos=tiempo1%100+tiempo2%100;
+            if(minutos>=60){
+                horas += minutos/60;
+                minutos%=60;
+            }
+        int total=horas*100+minutos;
+        if(total>=2400 || total%100>=60)
+            cout<<"la suma de los tiempos es invalida"<<endl;
             else{
-                int horas=hora1/100+hora2/100;
-                int minutos=hora1%100+hora2%100;
-                if(minutos>=60){
-                    horas += minutos/60;
-                    minutos%=60;
-                }
-                int resultado=horas*100+minutos;
-                if(resultado>=2400 || resultado%100>=60)
-                    cout<<"la suma de los tiempos es invalida"<<endl;
-                else{
-                    cout<<"la hora es "<<resultado<<endl;
+                cout<<"la hora es "<<total<<endl;
                 }
             }
         }
@@ -131,38 +126,33 @@ int main()
     case 5:
     {cout << "ejecutando el problema 5..."<< endl;
         int filas;
-
-        cout << "Ingrese un numero impar para el patron: ";
-        cin >> filas;
-
-        if (filas % 2 == 0) {
-            cout << "El numero ingresado no es impar. Por favor, ingrese un numero impar." << endl;
+        cout<<"ingrese un numero impar para el patron: ";
+        cin>>filas;
+        if (filas%2 == 0){
+            cout<<"el numero ingresado no es impar. Por favor, ingrese un numero impar."<<endl;
             return 1;
         }
+        for(int i=1; i<=filas; i+=2){
 
-        for (int i = 1; i <= filas; i += 2) {
-
-            for (int j = (filas - i) / 2; j > 0; j--) {
+            for(int j=(filas-i)/2; j>0; j--){
                 cout << " ";
             }
 
-            for (int k = 0; k < i; k++) {
-                cout << "*";
+            for(int k=0; k<i; k++){
+                cout<<"*";
             }
-            cout << endl;
+            cout<<endl;
         }
+        for(int i=filas-2; i>0; i-=2){
 
-
-        for (int i = filas - 2; i > 0; i -= 2) {
-
-            for (int j = (filas - i) / 2; j > 0; j--) {
-                cout << " ";
+            for(int j=(filas-i)/2; j>0; j--){
+                cout<<" ";
             }
 
-            for (int k = 0; k < i; k++) {
-                cout << "*";
+            for(int k=0; k<i; k++){
+                cout<<"*";
             }
-            cout << endl;
+            cout<<endl;
         }
 
         return 0;
@@ -180,21 +170,22 @@ int main()
         cout<<"ingrese un numero: ";
         cin>>n;
 
-        int a = 1, b = 1, suma_pares = 0;
+        int a=1, b=1, sumpar=0;
 
-        while (true) {
-            int siguiente = a + b;
-            if (siguiente >= n) break;
+        while(true){
+            int siguiente = a+b;
+            if(siguiente >= n)
+                break;
 
-            if (siguiente % 2 == 0) {
-                suma_pares += siguiente;
+            if(siguiente %2==0){
+                sumpar += siguiente;
             }
 
             a = b;
             b = siguiente;
         }
 
-        cout<<"el resultado de la suma es: "<<suma_pares<<endl;
+        cout<<"el resultado de la suma es: "<<sumpar<<endl;
 
         return 0;
     }
@@ -207,24 +198,24 @@ int main()
     case 9:
     {cout << "ejecutando el problema 9..."<< endl;
         int N;
-        cout << "ingrese un numero entero: "<<endl;
-        cin >> N;
+        cout<<"ingrese un numero entero: "<<endl;
+        cin>>N;
 
-        int suma_total = 0;
+        int sumtotal=0;
 
-        while (N > 0) {
-            int digito = N % 10;
-            int potencia = 1;
+        while(N>0){
+            int digito =N%10;
+            int potencia =1;
 
-            for (int i = 0; i < digito; i++) {
+            for(int i=0; i<digito; i++){
                 potencia *= digito;
             }
 
-            suma_total += potencia;
+            sumtotal += potencia;
             N /= 10;
         }
 
-        cout << "El resultado de la suma es: " << suma_total << endl;
+        cout<<"El resultado de la suma es: "<<sumtotal<<endl;
 
         return 0;
 
@@ -232,15 +223,7 @@ int main()
         break;
     case 10:
     {cout << "ejecutando el problema 10..."<< endl;
-        int n;
-        cout<<"Ingrese el valor de n: ";
-        cin >> n;
 
-        int enesimoPrimo = encontrarEnesimoPrimo(n);
-
-        cout<<"El primo numero "<<n<<" es: "<<enesimoPrimo<<endl;
-
-        return 0;
     }
         break;
     case 11:
@@ -248,10 +231,10 @@ int main()
         int numero;
 
         cout<<"ingrese un numero: ";
-        cin >> numero;
+        cin>>numero;
 
         int mcm = 1;
-        for (int i = 2; i <= numero; ++i) {
+        for(int i=2; i<=numero; ++i){
             mcm = CalcularMCM(mcm, i);
         }
         cout<<"el minimo comun multiplo es: "<<mcm<<endl;
@@ -263,11 +246,11 @@ int main()
     case 12:
     {cout << "ejecutando el problema 12..."<< endl;
         int n;
-        cout << "ingrese un numero: ";
-        cin >> n;
+        cout<<"ingrese un numero: ";
+        cin>>n;
 
-        int mayorPrimo = MaximoPrimo(n);
-        cout << "el mayor factor primo de " << n << " es: " << mayorPrimo << endl;
+        int mayorPrimo = MaxPrimo(n);
+        cout<<"el mayor factor primo de "<<n<<" es: "<<mayorPrimo<<endl;
 
         return 0;
     }
@@ -294,13 +277,7 @@ int main()
         break;
     case 17:
     {cout << "ejecutando el problema 17..."<< endl;
-        int k;
-        cout << "ingrese el numero: ";
-        cin >> k;
 
-        encontrarNumeroTriangular(k);
-
-        return 0;
     }
         break;
     default:
@@ -349,89 +326,32 @@ int MCD(int A, int B) {
 int CalcularMCM(int A, int B) {
     return A * B / MCD(A, B);
 }
-int numeroTriangular(int n) {
-    return n * (n + 1) / 2;
-}
-
-int contarDivisores(int num) {
-    int contador = 0;
-    for (int i = 1; i * i <= num; i++) {
-        if (num % i == 0) {
-            if (i * i == num) {
-                contador += 1;
-            } else {
-                contador += 2;
-            }
-        }
-    }
-    return contador;
-}
-
-void encontrarNumeroTriangular(int k) {
-    int n = 1, triangular, divisores;
-
-    while (true) {
-        triangular = numeroTriangular(n);
-        divisores = contarDivisores(triangular);
-        if (divisores > k) {
-            cout << "El numero es: " << triangular << " que tiene " << divisores << " divisores." << endl;
-            break;
-        }
-        n++;
-    }
-}
-int MaximoPrimo(int n) {
-    int max_primo = 0;
-    while (n % 2 == 0) {
-        max_primo = 2;
+int MaxPrimo(int n){
+    int maxprimo=0;
+    while(n % 2==0){
+        maxprimo=2;
         n /= 2;
     }
-    for (int i = 3; i * i <= n; i += 2) {
-        if (esPrimo(i)) {
-            while (n % i == 0) {
-                max_primo = i;
+    for(int i=3; i*i<=n; i += 2){
+        if(Primo(i)){
+            while(n % i == 0){
+                maxprimo=i;
                 n /= i;
             }
         }
     }
-    if (n > 2) {
-        max_primo = n;
+    if(n>2){
+        maxprimo=n;
     }
-
-    return max_primo;
+    return maxprimo;
 }
-bool esPrimo(int num) {
-    if (num <= 1)
+bool Primo(int num){
+    if(num<=1)
         return false;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0)
+    for(int i=2; i*i<=num; i++){
+        if(num % i==0)
             return false;
     }
     return true;
 }
-bool Primo(int numero) {
-    if (numero <= 1) return false;
-    if (numero == 2 || numero == 3) return true;
-    if (numero % 2 == 0 || numero % 3 == 0) return false;
 
-    for (int i = 5; i * i <= numero; i += 6) {
-        if (numero % i == 0 || numero % (i + 2) == 0) return false;
-    }
-
-    return true;
-}
-
-int encontrarEnesimoPrimo(int n) {
-    int contador = 0;
-    int numero = 2;
-
-    while (true) {
-        if (Primo(numero)) {
-            contador++;
-            if (contador == n) {
-                return numero;
-            }
-        }
-        numero++;
-    }
-}
